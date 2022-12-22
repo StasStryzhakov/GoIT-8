a51ed6939b80aa41d3f8e284ac39c57dcd6ae035
StasStryzhakov <kriegehund@gmail.com>
2022-12-22 14:25:57 +0200

    base version of pabot

---
+++ pabot/pabot/AdressBook.py
@@ -0,0 +1,179 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+# клас батько, для роботи з данними 
+class Field:
+    def __init__(self, value: str):
+        self._value = None
+        self.value = value
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        self._value = value
+        
+# клас нащадок, зберігає ім'я контакту, то провіряє на валідність   
+class Name(Field):
+    
+    @Field.value.setter
+    def value(self, name: str):
+        if name.isnumeric():
+            raise ValueError('Wrong name')
+        self._value = name
+        
+# клас нащадок, зберігає номер контакту, провіряє на валідність і форматує          
+class Phone(Field):
+    
+    @Field.value.setter
+    def value(self, phone: str):
+        if not phone.isnumeric():
+            raise ValueError('Wrong phones')
+        format_phone = self.format_phone_number(phone)
+        if format_phone:
+            self._value = format_phone
+        
+    @staticmethod
+    def format_phone_number(phone: str):
+        if len(phone) == 10:
+            return f'+38{phone}'
+        elif len(phone) == 12:
+            return f'+{phone}'
+        else:
+            raise ValueError('Wrong phones')    
+    
+# клас нащадок,  зберігає дату народження контакту, та перевіряє на валідність
+class Birthday(Field):
+    
+    @Field.value.setter
+    def value(self, birthday: str):
+        current_date = datetime.now().date()
+        birthday_date = datetime.strptime(birthday, '%Y-%m-%d').date()
+        if birthday_date > current_date:
+            raise ValueError("Your contact havent born yet")
+        self._value = birthday
+        
+# клас для роботи з обєктами класів нащадків Field       
+class Record:
+    
+    def __init__(self, name: str):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+        
+# вивід усієї доступної інформації про контакт        
+    def get_info(self):
+        birthday_info = ''
+        phones_info = [phone.value for phone in self.phones]
+        
+        if self.birthday:
+            birthday_info = f' Birthday: {self.birthday.value}'
+            
+        return f'{self.name.value} : {", ".join(phones_info)}{birthday_info}'
+    
+# розрахунок кількості днів до дня народження контакту            
+    def day_to_bithday(self):
+        if not self.birthday:
+            raise ValueError('This contact havent birthday date')
+        
+        current_date = datetime.now().date()
+        
+        birthday_date = datetime.strptime(self.birthday.value, '%Y-%m-%d').date()
+        this_year_birthday = birthday_date.replace(year=current_date.year)
+        
+        if this_year_birthday < current_date:
+           this_year_birthday = this_year_birthday.replace(year=current_date.year + 1)
+            
+        return (this_year_birthday - current_date).days
+    
+# додати телефон у список номерів        
+    def add_phone(self, phone: str):
+        self.phones.append(Phone(phone))
+        
+# додати дату народження       
+    def add_birthday(self, date: str):
+        self.birthday = Birthday(date)
+        
+# видалити телефон  
+    def delete_phone(self, phone: str):
+        for record_phone in self.phones:
+            if record_phone.value == phone:
+                self.phones.remove(record_phone)
+                return True
+        return False
+    
+# замінити телефон
+    def change_phone(self, phones):
+        for phone in phones:
+            if not self.delete_phone(phone):
+                self.add_phone(phone)
+    
+        
+    
+
+class AdressBook(UserDict):
+# загрузка стану із файла    
+    def __init__(self):
+        super().__init__()
+        self.load_from_file()
+        
+# додати контак до книги із ключем ім'я контакту    
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+# повертає список ісіх обєктів класу Record
+    def get_all_record(self):
+        return self.data
+    
+# перевірка на існування обєкта по ключу
+    def has_record(self, name):
+        return bool(self.data.get(name))
+
+# повертає обєкт класу Record по ключу
+    def get_record(self, name) -> Record:
+        return self.data.get(name)
+
+# видаляє обєкт класу Record
+    def remove_record(self, name):
+        del self.data[name]
+        
+# шукає контакт по введеним даним користувача в іменах та номерах телефону
+    def search(self, value):
+        if self.has_record(value):
+            return self.get_record(value)
+
+        for record in self.get_all_record().values():
+            for phone in record.phones:
+                if phone.value == value:
+                    return record
+
+        raise ValueError("Contact with this value does not exist.")
+    
+# виводить контакти по 3 на сторінку   
+    def iterator(self, count=3):
+        result = []
+        
+        for contact in self.data.values():
+            result.append(contact)
+            if len(result) == count:
+                yield result
+                result = []
+        
+        if result:
+            yield result
+
+# зберігання контактів у файл          
+    def save_to_file(self):
+        with open('AdressBook.bin', 'wb') as fw:
+            pickle.dump(self.data, fw)
+            
+# загрузка контактів із файлу
+    def load_from_file(self):
+        try:
+            with open('AdressBook.bin', 'rb') as fr:
+                self.data = pickle.load(fr)
+        except FileNotFoundError:
+            pass
---
+++ pabot/pabot/pabot.py
@@ -0,0 +1,181 @@
+from AdressBook import AdressBook, Record
+from Message import (AddContactMessage,
+                     AddContactBirthdayMessage,
+                     ChangeContacPhonetMessage,
+                     DaysToBirthdayMessage,
+                     DeleteContactMessage,
+                     DeletePhoneMessage,
+                     GreetingMessage,
+                     HelpMessage,
+                     StopMessage)
+
+# книга контактів
+CONTACTS = AdressBook()
+
+# вивід інструкції
+def get_help():
+    return HelpMessage.get_message()
+
+# обробляє помилки
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:           
+            return func(*args, **kwargs)
+                        
+        except KeyError:
+            return 'This contact doesnt exist, please try again.'
+        except ValueError as exception:
+            return exception.args[0]
+        except IndexError:
+            return 'This contac cannot be added, it exists already'
+        except TypeError:
+            return 'Unknown command or parametrs, please try again.'
+                       
+    return inner    
+    
+# привітальне повідомлення
+def greeting():
+    return GreetingMessage.get_message()
+
+# прощальне повідомлення
+def stop_bot():
+    return StopMessage.get_message()
+
+# додати контакт, спочатку формуєся сам контакт, а потім додається у книгу
+def add_contact(data):
+    
+    name, phones = get_data_from_user(data)
+    
+    if name in CONTACTS:
+        raise ValueError('This contact already exist.')
+    
+    record = Record(name)
+    
+    for phone in phones:
+        record.add_phone(phone)
+        
+    CONTACTS.add_record(record)
+    
+    return AddContactMessage.get_message(name)
+
+# змінити номер(и) контакту
+def change_contact(data):
+    
+    name, phones = get_data_from_user(data)
+    record = CONTACTS[name]
+    record.change_phone(phones)
+    
+    return ChangeContacPhonetMessage.get_message(name)
+
+# пошук контакту
+def show_contact_phone(data: str):
+    return CONTACTS.search(data.strip()).get_info()
+    
+# вивід інформації по всім контактам    
+def show_all_contacts():
+    
+    result = [record.get_info() for page in CONTACTS.iterator() for record in page]
+    return '\n'.join(result)
+    
+# видалити телефон
+def del_phone(data):
+    
+    name, phone = data.strip().split(' ')
+    record = CONTACTS[name]
+    return DeletePhoneMessage.get_message(record.delete_phone(phone), name, phone)
+
+# видалити контакт
+def del_contact(data):
+    
+    name = data.strip()
+    CONTACTS.remove_record(name)
+    return DeleteContactMessage.get_message(name)
+
+# додати дату народження
+def add_birth(data):
+    
+    name, birthday = data.strip().split(' ')
+    record = CONTACTS[name]
+    record.add_birthday(birthday)
+    return AddContactBirthdayMessage.get_message(name, birthday)
+    
+# виводить кількість днів до дня народження контакту
+def days_to_birthday(data: str):
+    
+    record = CONTACTS[data.strip()]
+    name = record.name.value
+    return  DaysToBirthdayMessage.get_message(name, record.day_to_bithday())
+
+# список команд боту
+COMMANDS = {'hello': greeting,
+            'help': get_help,
+            'add': add_contact,
+            'change': change_contact,
+            'phone': show_contact_phone,
+            'show all': show_all_contacts,
+            'good bye': stop_bot,
+            'close': stop_bot,
+            'exit': stop_bot,
+            'delete phone': del_phone,
+            'delete': del_contact,
+            'birthday': add_birth,
+            'days to birthday': days_to_birthday}
+
+# стоп функція
+def break_func():
+    return 'Wrong enter'
+
+# обробка данних введених користувачем
+def get_data_from_user(data: str):
+    name, *phones = data.lower().strip().split(' ')
+ 
+    return name, phones
+
+# вибір команди, якщо такої нема, то буде виконана стоп функція
+def get_command(command):
+    return COMMANDS.get(command, break_func)
+
+# обробка запиту користувача
+@input_error
+def get_user_request(user_input: str):
+    command = ''
+    data = ''
+    
+    for key in COMMANDS:
+        if user_input.strip().lower().startswith(key):
+            command = key
+            data = user_input[len(key):]
+            break
+        
+    if data:
+        return get_command(command)(data)
+    return get_command(command)()
+
+
+  
+    
+# головна функція
+def main():
+    try:
+        
+        print(get_help())
+        while True:
+            
+            user_request = input('Wait for your command master: ')
+            result = get_user_request(user_request)
+            print(result)
+            
+            if result == 'Good bye!':
+                break
+    finally:
+        CONTACTS.save_to_file()
+    
+        
+        
+                
+        
+
+
+
+if __name__ == '__main__':
+    main()
